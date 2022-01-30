18283ccbfdea0c22ad5bb1c9b7e5d44cc44edb7d
Duy Anh <huynhduyanh123123@gmail.com>
2022-01-30 12:04:53 +0700

    Update post page

--- web/src/views/Post.vue
+++ web/src/views/Post.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="bg-white shadow-md shadow-slate-300 fixed z-10 left-0 top-0 w-screen p-3">
-    <img src="src/assets/das_logo.png" alt="" class="h-10 inline-flex" />
-    <span class="text-xl ml-5">THPT Dĩ An</span>
-  </div>
-  <div class="grid grid-cols-5 mt-36 mb-36">
-    <article class="col-start-2 col-span-3" v-if="this.loaded">
-      <header class="text-5xl">{{ post.title }}</header>
-      <div id="content" class="mt-10 break-words" v-html="post.content"></div>
-      <div class="border-t-2 border-t-slate-300 pt-10 mt-10 flex flex-row flex-wrap gap-3" v-if="post.attachments.length > 0">
-        <img v-for="att in post.attachments" class="max-w-md cursor-pointer hover:opacity-80" :src="serverBaseURL + '/static/' + att.id" alt="" @click="previewImage(att.id)" />
+  <Header></Header>
+  <div class="max-w-[1024px] m-auto pb-16">
+    <div class="flex flex-row gap-1 place-items-center">
+      <HomeIcon class="w-4 cursor-pointer" @click="backToHome"></HomeIcon>
+      <ChevronRightIcon class="w-4"></ChevronRightIcon>
+      <div class="cursor-pointer" @click="backToHome">Tin tức</div>
+      <ChevronRightIcon class="w-4"></ChevronRightIcon>
+    </div>
+    <div v-if="this.loaded">
+      <div class="text-right text-slate-500 mb-3">{{ new Intl.DateTimeFormat("vi-VN" , {timeStyle: "medium", dateStyle: "short"}).format(new Date(post.date)) }}</div>
+      <article class="border-y-2 border-y-slate-300 py-10 font-serif">
+        <header class="text-4xl">{{ post.title }}</header>
+        <section id="content" class="mt-5 break-words" v-html="post.content"></section>
+      </article>
+      <div class="mt-10 flex flex-row flex-wrap gap-3" v-if="post.attachments.length > 0">
+        <img v-for="att in post.attachments" class="max-w-xs cursor-pointer hover:opacity-80" :src="serverBaseURL + '/static/' + att.id" alt="" @click="previewImage(att.id)" />
       </div>
-    </article>
-    <div class="col-start-2 col-span-3" v-else>
-      <svg class="animate-spin h-16 w-16 text-sky-400 m-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+    </div>
+    <div v-else>
+      <svg class="animate-spin h-8 w-8 text-sky-400 m-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
         <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
         <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
       </svg>
@@ -23,9 +29,15 @@
     <ChevronDoubleUpIcon class="w-12 cursor-pointer border-slate-400 border-2 rounded-full text-slate-500 p-2" @click="jumpToTop"></ChevronDoubleUpIcon>
     <ChevronDoubleDownIcon class="w-12 cursor-pointer border-slate-400 border-2 rounded-full text-slate-500 p-2" @click="jumpToBottom"></ChevronDoubleDownIcon>
   </div>
-  <div v-if="previewImageId !== undefined" @click="previewImage(undefined)">
-    <div class="bg-black opacity-75 fixed top-0 left-0 w-screen h-screen"></div>
-    <img class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10" :src="serverBaseURL + '/static/' + previewImageId" alt="" />
+  <div v-if="previewImageId !== undefined">
+    <div class="bg-black opacity-75 fixed top-0 left-0 w-screen h-screen" @click="previewImage(undefined)"></div>
+    <div class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 select-none w-4/5">
+      <img :style="`width: ${this.previewImageSize}%`" class="m-auto transition-all	duration-300" :src="serverBaseURL + '/static/' + previewImageId" alt="" />
+      <div class="flex flex-row justify-center mt-1">
+        <ZoomInIcon class="w-7 cursor-pointer p-1 bg-gray-300 hover:bg-gray-400" @click="this.previewImageSize = Math.min(this.previewImageSize + 10, 80)"></ZoomInIcon>
+        <ZoomOutIcon class="w-7 cursor-pointer p-1 bg-gray-300 hover:bg-gray-400" @click="this.previewImageSize = Math.max(this.previewImageSize - 10, 50)"></ZoomOutIcon>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -33,15 +45,18 @@
 import { ChevronDoubleUpIcon, ChevronDoubleDownIcon, HomeIcon } from '@heroicons/vue/solid'
 import server from "../api/server";
 import conf from "../conf";
+import Header from "../components/Header.vue";
+import {ChevronRightIcon, ZoomInIcon, ZoomOutIcon} from "@heroicons/vue/outline";
 
 export default {
   name: "Post",
-  components: { ChevronDoubleUpIcon, ChevronDoubleDownIcon, HomeIcon },
+  components: {Header, ChevronRightIcon, ChevronDoubleUpIcon, ChevronDoubleDownIcon, HomeIcon, ZoomInIcon, ZoomOutIcon },
   data() {
     return {
       post: {},
       loaded: false,
-      previewImageId: undefined
+      previewImageId: undefined,
+      previewImageSize: 0
     }
   },
   computed: {
@@ -61,6 +76,7 @@ export default {
     },
     previewImage(id) {
       this.previewImageId = id
+      this.previewImageSize = 50
     }
   },
   mounted() {
