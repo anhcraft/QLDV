0fe229992c20ae4d4bbdee4505d7535cabe6af2a
Duy Anh <huynhduyanh123123@gmail.com>
2022-01-30 17:01:44 +0700

    Improved header

--- web/src/components/Header.vue
+++ web/src/components/Header.vue
@@ -6,13 +6,13 @@
         <img src="../assets/das_logo.png" class="w-12 h-12"  alt=""/>
         <p class="text-3xl ml-5 font-light">ĐOÀN THPT DĨ AN</p>
         <div class="absolute right-0">
-          <button v-if="!$root.isLoggedIn" @click="logIn()" class="transition-all duration-300 border-2 border-sky-500 rounded-md font-light hover:text-white hover:bg-sky-500 px-5 py-2 flex flex-row gap-2 place-items-center">
-            <img src="../assets/google.svg" class="w-6 h-6" alt="">
-            <span>Đăng nhập</span>
-          </button>
-          <button v-else @click="viewProfile()" class="transition-all duration-300 border-2 border-indigo-500 rounded-md font-light hover:text-white hover:bg-indigo-500 px-5 py-2">
-            <span>Trang cá nhân</span>
-          </button>
+          <div class="flex flex-row gap-10 place-items-center">
+            <router-link to="/">
+              <span class="text-lg border-b-2 border-b-2-white hover:border-b-gray-500">Trang chủ</span>
+            </router-link>
+            <button v-if="!$root.isLoggedIn" @click="logIn()" class="transition-all duration-500 rounded-md bg-indigo-500 hover:bg-indigo-600 px-4 py-2 text-white text-sm">Đăng nhập</button>
+            <button v-else @click="viewProfile()" class="transition-all duration-500 rounded-md bg-indigo-500 hover:bg-indigo-600 px-4 py-2 text-white text-sm">Trang cá nhân</button>
+          </div>
         </div>
       </div>
     </div>
