5d0064e528e5f89960c238978349a85fb4e05d11
Duy Anh <huynhduyanh123123@gmail.com>
2022-02-01 22:48:06 +0700

    Some display improvements to user profile page

--- web/src/views/Profile.vue
+++ web/src/views/Profile.vue
@@ -3,7 +3,7 @@
   <div class="pb-16 max-w-[1024px] m-auto">
     <Breadcrumb text="Trang cá nhân" :link="`/u/${this.$route.params.id}`"></Breadcrumb>
     <div class="grid grid-cols-3 gap-16 mt-5">
-      <div class="col-span-1 shadow-lg shadow-slate-400">
+      <div class="col-span-1 shadow-lg shadow-slate-400 self-start">
         <section>
           <div class="border-l-4 border-l-emerald-400 bg-emerald-100 px-4 py-2 shadow-lg shadow-slate-300">THÔNG TIN</div>
           <div class="px-5 py-3">
@@ -24,17 +24,33 @@
         </section>
         <section>
           <div class="border-l-4 border-l-emerald-400 bg-emerald-100 px-4 py-2 shadow-lg shadow-slate-300">THÀNH TÍCH</div>
-          <ul class="px-5 py-3 list-disc list-inside">
-            <li v-for="a in achievements">{{ a }}</li>
-          </ul>
+          <div class="px-5 py-3">
+            <div v-if="loadingProgression">
+              <svg class="animate-spin h-4 w-4 text-sky-400 m-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+              </svg>
+            </div>
+            <ul v-else class="list-disc list-inside">
+              <li v-for="a in achievements">{{ a }}</li>
+            </ul>
+          </div>
         </section>
         <section>
           <div class="border-l-4 border-l-emerald-400 bg-emerald-100 px-4 py-2 shadow-lg shadow-slate-300">XẾP LOẠI</div>
-          <ul class="px-5 py-3 list-disc list-inside">
-            <li v-for="(value, name) in rates">
-              {{ value === 1 ? "Tốt" : (value === 2 ? "Khá" : "-") }} ({{ name }} - {{ parseInt(name) + 1 }})
-            </li>
-          </ul>
+          <div class="px-5 py-3">
+            <div v-if="loadingProgression">
+              <svg class="animate-spin h-4 w-4 text-sky-400 m-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+              </svg>
+            </div>
+            <ul v-else class="list-disc list-inside">
+              <li v-for="(value, name) in rates">
+                {{ value === 1 ? "Tốt" : (value === 2 ? "Khá" : "-") }} ({{ name }} - {{ parseInt(name) + 1 }})
+              </li>
+            </ul>
+          </div>
         </section>
       </div>
       <div class="col-span-2">
@@ -121,7 +137,7 @@ export default {
   },
   computed: {
     isPersonalProfile() {
-      return this.$root.profile.email === this.getUserId
+      return !this.loadingProfile && this.$root.profile.email === this.getUserId()
     }
   },
   methods: {
@@ -181,7 +197,7 @@ export default {
       s["rates"].forEach((value) => {
         this.rates[value["year"]] = value["level"]
       })
-      this.progressionLoading = false
+      this.loadingProgression = false
     })
   }
 }
