9d0358361bb989c81fd294e2761a2319f327b189
Duy Anh <huynhduyanh123123@gmail.com>
2022-03-05 11:52:43 +0700

    Ready for production!!!

--- web/src/components/PostWidget.vue
+++ web/src/components/PostWidget.vue
@@ -10,7 +10,7 @@ export default {
 
 <template>
   <router-link class="pw-container block cursor-pointer w-full relative h-36" :to="'/p/' + id">
-    <img v-if="bg === undefined || bg.length === 0" src="src/assets/vn-coast.jpg" class="object-cover object-center w-full h-36" />
+    <img v-if="bg === undefined || bg.length === 0" src="../assets/vn-coast.jpg" class="object-cover object-center w-full h-36" />
     <img v-else :src="bg" class="object-cover object-center w-full h-36" />
     <div class="pw-image bg-black absolute w-full h-full top-0 left-0 opacity-20 transition delay-75 duration-300 ease-in-out"></div>
     <div class="w-full absolute bottom-3 pl-5 pr-5 z-10">
--- web/src/conf.ts
+++ web/src/conf.ts
@@ -8,7 +8,7 @@ const conf = {
         appId: "1:197333612896:web:d00de79ea57271d1504605",
         measurementId: "G-0LYF0592TD"
     },
-    server: "https://1969-2402-800-6310-e74d-5f2d-2ce-5ad8-928a.ngrok.io"
+    server: "https://api.thptdian.org"
 };
 
 export default conf;
