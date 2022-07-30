21523fd1b26e6fadc1af8c0adbae7097e1f33aab
anhcraft <huynhduyanh123123@gmail.com>
2022-07-31 00:41:53 +0700

    Adjusted not-found page

--- web/src/views/NotFoundPage.vue
+++ web/src/views/NotFoundPage.vue
@@ -2,9 +2,11 @@
   <Header></Header>
   <div class="py-32">
     <img src="../assets/tree-drawing.svg" class="m-auto max-w-[10rem] md:max-w-xs"  alt=""/>
-    <div class="text-2xl text-center font-light font-serif mt-5">
-      <p>Trang này không tồn tại</p>
-      <p>{{ $route.fullPath }}</p>
+    <div class="text-center font-light font-serif mt-10">
+      <p class="text-3xl mb-3">Trang này không tồn tại!</p>
+      <p class="text-2xl text-cyan-600 hover:underline">
+        <router-link to="/">Quay về trang chủ</router-link>
+      </p>
     </div>
   </div>
   <FloatingMenu></FloatingMenu>
