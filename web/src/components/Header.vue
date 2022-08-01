fa33ea91739db50cb1c684aaae031a7056da9d3c
anhcraft <huynhduyanh123123@gmail.com>
2022-08-01 08:52:10 +0700

    Fixed committee page link in the header

--- web/src/components/Header.vue
+++ web/src/components/Header.vue
@@ -18,7 +18,7 @@
               <div class="flex flex-row gap-8 items-center">
                 <router-link class="border-b-2 border-b-transparent hover:border-b-white" to="/">Trang chủ</router-link>
                 <router-link class="border-b-2 border-b-transparent hover:border-b-white" to="/p">Tin tức</router-link>
-                <router-link class="border-b-2 border-b-transparent hover:border-b-white" to="/about">Tổ chức</router-link>
+                <router-link class="border-b-2 border-b-transparent hover:border-b-white" to="/bch">Tổ chức</router-link>
                 <router-link class="border-b-2 border-b-transparent hover:border-b-white" to="/events">Hoạt động</router-link>
                 <button v-if="!$root.isLoggedIn()" @click="logIn()" class="btn-primary ml-10">Đăng nhập</button>
                 <button v-else @click="viewProfile()" class="btn-primary ml-10">Trang cá nhân</button>
