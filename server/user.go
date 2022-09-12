f1d235445f95264e75929abef31d077603a4940d
anhcraft <huynhduyanh123123@gmail.com>
2022-09-12 22:33:45 +0700

    Improved user code

--- server/user.go
+++ server/user.go
@@ -38,22 +38,64 @@ func (u *User) isRatePublic() bool {
 	return (u.ProfileSettings & 8) == 8
 }
 
+// hasPrivilegeOver Checks whether the current user has superior privilege to the other
+// Groups:
+// - Guest
+// - Member: Regular Member, Certified Member
+// - Class Managers: Class Secretary, Class Deputy Secretary
+// - Global Managers: Secretary, Deputy Secretary
+// Rules:
+// - The user gains himself privilege
+// - Roles in the same group are not overlapped each other
+// Mode:
+// - 0 (Group-Distinct) Global Managers > Class Managers > Member > Guest (default)
+// - 1 (Class-Distinct) Global Managers > Class Managers - Member - Guest
+func (u *User) hasPrivilegeOver(who *User, mode uint8) bool {
+	if u.ID == who.ID {
+		return true
+	}
+	distinctGroupTest := GetRoleGroup(u.Role)-GetRoleGroup(who.Role) > 0
+	if mode == 1 {
+		return distinctGroupTest && GetRoleGroup(u.Role) == RoleGroupGlobalManager
+	} else {
+		return distinctGroupTest
+	}
+}
+
+// serialize Serializes this user's data into a gabs container
+// Information may be hidden due to privacy concerns.
+// - Common information: id, profileSettings, profileCover, profileBoard
+// - Personal information: name, gender, entryYear, class, role
+//    (*) At least one requirement met to gain access:
+//      + Be the user himself
+//      + The profile is unlocked
+//      + If the profile is locked, the requester must be in the manager group
+// - Secret information: email, birthday, phone
+//    (*) At least one requirement met to gain access:
+//      + Be the user himself
+//      + The requester must be in the manager group
 func (u *User) serialize(requester *User) *gabs.Container {
 	res := gabs.New()
 	_, _ = res.Set(u.ID, "id")
-	_, _ = res.Set(u.Name, "name")
-	if requester != nil && IsLoggedIn(requester.Role) {
-		_, _ = res.Set(u.ProfileSettings, "profileSettings")
-		_, _ = res.Set(u.Email, "email")
-		_, _ = res.Set(u.Role, "role")
+	_, _ = res.Set(u.ProfileSettings, "profileSettings")
+	_, _ = res.Set(u.ProfileCover, "profileCover")
+	_, _ = res.Set(u.ProfileBoard, "profileBoard")
 
+	// Group-distinct check
+	accessLocked := requester.hasPrivilegeOver(u, 0)
+	// Personal information
+	if accessLocked || !u.isProfileLocked() {
+		_, _ = res.Set(u.Name, "name")
 		_, _ = res.Set(u.Gender, "gender")
-		_, _ = res.Set(u.EntryYear, "entry")
-		_, _ = res.Set(u.ProfileCover, "profileCover")
-		_, _ = res.Set(u.ProfileBoard, "profileBoard")
-		if full || u.isClassPublic() {
-			_, _ = res.Set(u.Class, "class")
-		}
+		_, _ = res.Set(u.EntryYear, "entryYear")
+		_, _ = res.Set(u.Class, "class")
+		_, _ = res.Set(u.Role, "role")
+	}
+	// Secret information
+	if accessLocked {
+		_, _ = res.Set(u.Email, "email")
+		_, _ = res.Set(u.Birthday, "birthday")
+		_, _ = res.Set(u.Phone, "phone")
 	}
 	return res
 }
